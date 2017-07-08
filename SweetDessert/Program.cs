eb6078d22ee47b708eecf7b3e4e7fa31d189c758
Stranger <jmanova83@gmail.com>
2017-07-08 10:50:59 +0300

    1 done

---
+++ SweetDessert/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweetDessert
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var moneyAvailable = decimal.Parse(Console.ReadLine());
+			var guests = int.Parse(Console.ReadLine());
+			var bananaPrice = decimal.Parse(Console.ReadLine());
+			var eggPrice = decimal.Parse(Console.ReadLine());
+			var berriesPrice = decimal.Parse(Console.ReadLine());
+
+			int portions = (int)Math.Ceiling(guests / 6.00);
+
+			var bananasNeeded = portions * 2;
+			var eggsNeeded = portions * 4;
+			decimal berriesNeeded = (decimal)(portions * 0.2);
+
+			decimal moneyNeeded = bananasNeeded * bananaPrice + eggsNeeded * eggPrice +
+				berriesNeeded * berriesPrice;
+			if (moneyAvailable >= moneyNeeded)
+			{
+				Console.WriteLine($"Ivancho has enough money - it would cost {moneyNeeded:f2}lv.");
+			}
+			else
+			{
+				Console.WriteLine($"Ivancho will have to withdraw money - he will need {(moneyNeeded - moneyAvailable):f2}lv more.");
+			}
+		}
+	}
+}
